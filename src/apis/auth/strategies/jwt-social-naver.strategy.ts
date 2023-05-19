81bbcbd4d2e8884058415deeb8f69d179075826d
jeongheejun <jeonhj7590@gmail.com>
2023-05-19 21:25:11 +0900

    docs: 라이브러리 버전문제 수정

--- src/apis/auth/strategies/jwt-social-naver.strategy.ts
+++ src/apis/auth/strategies/jwt-social-naver.strategy.ts
@@ -1,5 +1,5 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-naver';
+import { Strategy } from 'passport-naver-v2';
 
 export class JwtNaverStrategy extends PassportStrategy(Strategy, 'naver') {
   constructor() {
