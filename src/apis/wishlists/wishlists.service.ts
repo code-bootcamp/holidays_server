bcfb43909bbc8e689be406486c16e4704bc91c6d
jeongheejun <jeonhj7590@gmail.com>
2023-05-20 16:02:23 +0900

    feat: 위시리스트 생성,조회,삭제 추가

--- src/apis/wishlists/wishlists.service.ts
+++ src/apis/wishlists/wishlists.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { FetchWishlists } from './dto/fetch-wishlists.output';
 import { Wishlist } from './entities/wishlist.entity';
+import {
+  IWishlistsServiceCreate,
+  IWishlistsServiceDelete,
+  IWishlistsServiceFindOneByUserId,
+} from './interfaces/wishlists-service.interface';
 
 @Injectable()
 export class WishlistsService {
@@ -10,15 +16,44 @@ export class WishlistsService {
     private readonly wishlistsRepository: Repository<Wishlist>, //
   ) {}
 
-  findOneByUserId({ user_id }): Promise<Wishlist[]> {
-    return this.wishlistsRepository.find({ where: { user_: user_id } });
+  async findOneByUserId({
+    user_id,
+  }: IWishlistsServiceFindOneByUserId): Promise<FetchWishlists[]> {
+    const result = await this.wishlistsRepository
+      .createQueryBuilder('wishlist')
+      .select([
+        'c.class_id AS class_id',
+        'c.title AS title',
+        'c.price AS price',
+        'c.total_time AS total_time',
+        'c.address AS address',
+        'c.address_detail AS address_detail',
+        'i.url AS url',
+      ])
+      .innerJoin('user', 'u', 'wishlist.user_userId = u.user_id')
+      .innerJoin('class', 'c', 'wishlist.user_userId = c.user_userId')
+      .innerJoin('image', 'i', 'c.class_id = i.class_classId')
+      .where('1=1')
+      .andWhere('u.user_id = :user_id', { user_id })
+      .getRawMany();
+    console.log(result);
+
+    return result;
   }
 
-  create({ user_id, class_id }) {
-    return '찜하기 성공!';
+  async create({
+    user_id,
+    class_id,
+  }: IWishlistsServiceCreate): Promise<Wishlist> {
+    const result = await this.wishlistsRepository.save({
+      user_: { user_id },
+      class_: { class_id },
+    });
+
+    return result;
   }
 
-  async delete({ wishlist_id }): Promise<boolean> {
+  async delete({ wishlist_id }: IWishlistsServiceDelete): Promise<boolean> {
     const result = await this.wishlistsRepository.delete({ wishlist_id });
 
     return result.affected ? true : false;
